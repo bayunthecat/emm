bbc046b68e4ca3d20dbf9dee944f62864d0291f6
User <User>
2017-02-20 09:19:27 +0200

    Initial comit

---
+++ src/main/java/com/test/emm/controller/TestAction.java
@@ -0,0 +1,19 @@
+package com.test.emm.controller;
+
+public class TestAction {
+
+    private String name;
+
+    public String execute() throws Exception {
+        return "success";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
